dd1926d5e802650af94933a94e586c22d5389ca2
Filip Jerga <jerga99@gmail.com>
2021-01-13 09:50:04 +0100

    Elevators Movement

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private float m_AccuTime = 0;
+    private float m_RunnigTime = 3.0f;
+    private float m_Speed = 5.0f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_AccuTime += Time.deltaTime;
+
+        if (m_AccuTime >= m_RunnigTime)
+        {
+            ReverseElevator();
+        } else
+        {
+            transform.Translate(0, m_Speed * Time.deltaTime, 0);
+        }
+    }
+
+    private void ReverseElevator()
+    {
+        m_AccuTime = 0;
+        m_Speed = -m_Speed;
     }
 }
