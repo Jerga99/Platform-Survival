dd8f63619f09d8c4c29043b496ac91ace4b05d2c
Filip Jerga <jerga99@gmail.com>
2021-01-18 11:06:31 +0000

    Enemy follow target

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody m_Rb;
+    private GameObject m_FollowTarget;
+
+    private void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Starting enemy controller!");
+        m_FollowTarget = GameObject.Find("Player");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 moveTowards = (m_FollowTarget.transform.position - transform.position).normalized;
+
+        m_Rb.AddForce(moveTowards * speed);
         
     }
 }
