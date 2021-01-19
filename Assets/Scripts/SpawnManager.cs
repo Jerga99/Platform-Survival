1d55e22bb3786bcf4374d4a5f668f684f45b575e
Filip Jerga <jerga99@gmail.com>
2021-01-19 09:58:21 +0000

    Spawn enemy waves

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     public Vector2 spawnRange;
 
     private int m_EnemyCount;
+    private int m_waves;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_waves = 1;
         SpawnEnemy();   
     }
 
@@ -21,17 +23,21 @@ public class SpawnManager : MonoBehaviour
 
         if (m_EnemyCount == 0)
         {
+            m_waves++;
             SpawnEnemy();
         }
     }
 
     private void SpawnEnemy()
     {
-        Vector3 spawnPosition = new Vector3(
-            Random.Range(spawnRange[0], spawnRange[1]),
-            enemyPrefab.transform.position.y,
-            Random.Range(spawnRange[0], spawnRange[1]));
+        for (var i = 0; i < m_waves; i++)
+        {
+            Vector3 spawnPosition = new Vector3(
+                Random.Range(spawnRange[0], spawnRange[1]),
+                enemyPrefab.transform.position.y,
+                Random.Range(spawnRange[0], spawnRange[1]));
 
-        Instantiate(enemyPrefab, spawnPosition, enemyPrefab.transform.rotation);
+            Instantiate(enemyPrefab, spawnPosition, enemyPrefab.transform.rotation);
+        }
     }
 }
